cf68738f19d6b9ceaf2cbccdcc69f71d3b08ebb4
Харченко Евгений Викторович <ip012s31@bwp14.csc.local>
2021-03-20 13:12:43 +0700

    Create file

---
+++ chess.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"r n b q k b n r"<<endl;
+  cout<<"p p p p p p p p"<<endl<<endl<<endl<<endl<<endl;
+  cout<<"p p p p p p p p"<<endl;
+  cout<<"r n b q k b n r"<<endl;
+  return 0;
+}
